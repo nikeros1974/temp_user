66a2bb8e3742311df1990a6cff943cceb33f5da2
nikeros1974 <michelepatron@gmail.com>
2020-08-03 23:48:13 +0100

    Other change to setup.py

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/nikeros1974/temp_user",
     dependency_links=[
-        'git+https://github.com/nikeros1974/temp_library'
+        'git+https://github.com/nikeros1974/temp_library#egg=temp_library'
     ],
     packages=setuptools.find_packages(),
     classifiers=[
