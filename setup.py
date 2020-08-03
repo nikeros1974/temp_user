5bd0319f9fca6d1f4722bc4577430c9f3dfbf584
nikeros1974 <michelepatron@gmail.com>
2020-08-04 00:08:58 +0100

    Minor change to setup.py

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setuptools.setup(
     url="https://github.com/nikeros1974/temp_user",
     install_requires=['temp_library',],
     dependency_links=[
-        'git+https://github.com/nikeros1974/temp_library/tarball/master'
+        'https://github.com/nikeros1974/temp_library/tarball/master',
     ],
     packages=setuptools.find_packages(),
     classifiers=[
