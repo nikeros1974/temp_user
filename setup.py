8957797fc1e578a6f1a09f79d2b353c93cefe942
nikeros1974 <michelepatron@gmail.com>
2020-08-04 00:00:49 +0100

    Minor change to setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setuptools.setup(
     long_description="long description",
     long_description_content_type="text/markdown",
     url="https://github.com/nikeros1974/temp_user",
-    install_requires=['temp_library',],
+    install_requires=['temp_library==0.0.1',],
     dependency_links=[
         'git+https://github.com/nikeros1974/temp_library.git#egg=temp_library'
     ],
